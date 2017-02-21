6826c7479405c546a1c968bc3284c04bf319e7cd
zl6114 <frankzihanliu@hotmail.com>
2017-02-21 11:58:54 +0000

    test

--- parser/header.hpp
+++ parser/header.hpp
@@ -2,37 +2,6 @@
 #define header_hpp
 #include <string>
 
-enum TokenType{
-    T_VARIABLE = 0;
-    T_CONTROL_FLOW = 1;
-    T_INT = 2;
-    T_SIMICOLOUMN = 3;
-    T_LBRACKET = 4;
-    T_RBRACKET = 5;
-    T_LCURLYBRACKET = 6;
-    T_RCURLYBRACKET = 7;
-};
-
-union TokenValue{
-    double numberValue;
-    std::string *wordValue;
-};
-
-
-// This is a global variable used to move the
-// attribute value from the lexer back to the
-// main program.
-// By convention it is called yylval, as that is
-// the name that will be automatially generated
-// by Bison (see next lab).
-extern TokenValue yylval;
-extern int LineNum;
-extern int ColNum;
-extern std::string SourceFile;
-
-// This is the lexer function defined by flex. Each
-// time it is called, a token type value will be
-// returned.
-extern int yylex();
+extern const Program *parseAST();
 
 #endif
